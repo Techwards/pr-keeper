ce7df3fb9e4a9f74588238a8fdcdc476901d6271
sheraz064-ai <sherazsiddiqui0456@gmail.com>
2023-01-02 11:25:41 +0500

    feat(PRK-16): called the running function to handle async function

--- dist/index.js
+++ dist/index.js
@@ -6,16 +6,32 @@ require('./sourcemap-register.js');/******/ (() => { // webpackBootstrap
 
 "use strict";
 
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
 };
 Object.defineProperty(exports, "__esModule", ({ value: true }));
-const core_1 = __importDefault(__nccwpck_require__(186));
+const core = __importStar(__nccwpck_require__(186));
 function run() {
     try {
         // const pullRequest = github.context.payload.pull_request
         // const client = github.getOctokit(core.getInput('token'))
-        core_1.default.info('client');
+        core.info('client');
         // if (pullRequest) {
         //   const owner = pullRequest.base.user.login
         //   const repo = pullRequest.base.repo.name
@@ -61,7 +77,7 @@ function run() {
         // }
     }
     catch (error) {
-        core_1.default.setFailed(getErrorMessage(error));
+        core.setFailed(getErrorMessage(error));
     }
 }
 function validatePRField(data) {
