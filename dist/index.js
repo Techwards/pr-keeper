3dc3a6d4443a8916507840f09c046ad59d27f1f5
sheraz064-ai <sherazsiddiqui0456@gmail.com>
2023-01-02 10:50:44 +0500

    feat(PRK-16): commented code to check the error

--- dist/index.js
+++ dist/index.js
@@ -32,7 +32,7 @@ function run() {
         // const pullRequest = github.context.payload.pull_request
         // const client = github.getOctokit(core.getInput('token'))
         core.info('client');
-        // throw new Error('error')
+        throw new Error('error');
         // if (pullRequest) {
         //   const owner = pullRequest.base.user.login
         //   const repo = pullRequest.base.repo.name
